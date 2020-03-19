ce5008af70550b1e7a7e6aedded51c774d41fad3
mitsu1119 <hachimitsu1119@gmail.com>
2020-03-20 00:40:48 +0900

    ws3

--- 2020/angstromCTF/Califrobnication/getSt.py
+++ 2020/angstromCTF/Califrobnication/getSt.py
@@ -1,8 +1,6 @@
-
-for i in range(2, 3):
-    with open("c{}".format(i), "rb") as f:
-        buf = f.read()[len("Here's your encrypted flag: "):-1]
-    res = ""
-    for i in buf:
-        res += chr(42 ^ i)
-    print(res)
+with open("c2", "rb") as f:
+    buf = f.read()[len("Here's your encrypted flag: "):-1]
+res = ""
+for i in buf:
+    res += chr(42 ^ i)
+print(res)
